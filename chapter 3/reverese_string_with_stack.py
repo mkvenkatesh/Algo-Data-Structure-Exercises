2e426bf244d0fdf36031e88fd571863d61b326a2
Venkatesh Mandalapa <venkatesh.mandalapa@gmail.com>
2020-10-17 18:33:59 -0400

    Use the stack class

--- chapter 3/reverese_string_with_stack.py
+++ chapter 3/reverese_string_with_stack.py
@@ -2,24 +2,7 @@
 Reverse a string using a Stack
 '''
 
-class Stack:
-    def __init__(self):
-        self.s = []
-
-    def push(self, item):
-        self.s.append(item)
-
-    def pop(self):
-        return self.s.pop()
-
-    def peek(self):
-        return self.s[-1]
-    
-    def is_empty(self):
-        return self.s == []
-
-    def size(self):
-        return len(self.s)
+from stack import Stack
 
 str_to_reverse = "intercodes"
 reversed_str = ""
