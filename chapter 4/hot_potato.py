5f27206f4a8958a4b6850a325618bf47e9e64d75
Venkatesh Mandalapa <venkatesh.mandalapa@gmail.com>
2020-10-22 07:49:34 -0400

    Code for queue implementation and hot potato game using queue

---
+++ chapter 4/hot_potato.py
@@ -0,0 +1,40 @@
+'''
+Hot Potato
+
+We will implement a general simulation of Hot Potato. Our program will input a
+list of names and a constant, call it “num” to be used for counting. It will
+return the name of the last person remaining after repetitive counting by num.
+What happens at that point is up to you. 
+
+To simulate the circle, we will use a queue. Assume that the child holding the
+potato will be at the front of the queue. Upon passing the potato, the
+simulation will simply dequeue and then immediately enqueue that child, putting
+her at the end of the line. She will then wait until all the others have been at
+the front before it will be her turn again. After num dequeue/enqueue
+operations, the child at the front will be removed permanently and another cycle
+will begin. This process will continue until only one name remains (the size of
+the queue is 1).
+'''
+
+from queue import Queue
+from random import randrange
+
+def hot_potato(q, num):
+    q = Queue()
+    for name in name_list:
+        q.enqueue(name)
+
+    while q.size() != 1:
+        for i in range(num):
+            # take the first element and push it to the rear
+            q.enqueue(q.dequeue())
+        # hot potato! remove the first person from the queue
+        q.dequeue()
+
+    return q.dequeue()
+
+if __name__ == "__main__":
+    name_list = ["John", "Jack", "Jill", "Jake", "Jane", "Jumbo", "Jimathy"]
+    num = randrange(0, 100)
+    print("Random num:", num)
+    print("Last remaining person is:", hot_potato(name_list, num))
---
+++ chapter 4/queue.py
@@ -0,0 +1,56 @@
+'''
+Queues
+
+The queue abstract data type is defined by the following structure and
+operations. A queue is structured, as described above, as an ordered collection
+of items which are added at one end, called the “rear,” and removed from the
+other end, called the “front.” Queues maintain a FIFO ordering property. The
+queue operations are given below.
+
+• Queue() creates a new queue that is empty. It needs no parameters and returns
+an empty queue.
+• enqueue(item) adds a new item to the rear of the queue. It needs the item and
+returns nothing.
+• dequeue() removes the front item from the queue. It needs no parameters and
+returns the item. The queue is modified.
+• is_empty() tests to see whether the queue is empty. It needs no parameters and
+returns a boolean value.
+• size() returns the number of items in the queue. It needs no parameters and
+returns an integer.
+
+'''
+
+class Queue:
+    def __init__(self):
+        self.q = []
+
+    def enqueue(self, item):
+        self.q.append(item)
+    
+    def dequeue(self):
+        return self.q.pop(0)
+
+    def size(self):
+        return len(self.q)
+    
+    def is_empty(self):
+        return self.q == []
+
+if __name__ == "__main__":
+    q = Queue()
+    
+    print("Queue is empty?:", q.is_empty())
+
+    print("Enqueue: hello")
+    q.enqueue("hello")
+
+    print("Enqueue: world")
+    q.enqueue("world")
+
+    print("Enqueue: 33")
+    q.enqueue(33)
+
+    print("Queue size:", q.size())
+    print("Dequeue:", q.dequeue())
+    print("Queue size:", q.size())
+    print("Queue is empty?:", q.is_empty())
