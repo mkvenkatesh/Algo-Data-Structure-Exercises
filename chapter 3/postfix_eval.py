f38e1986ab5c8a2fdbbb33cdcdbb4348dfb76381
Venkatesh Mandalapa <venkatesh.mandalapa@gmail.com>
2020-11-03 06:58:29 -0500

    add input validation

--- chapter 3/postfix_eval.py
+++ chapter 3/postfix_eval.py
@@ -20,25 +20,60 @@ the stack. Pop the operand_stack and return the value.
 
 from stack import Stack
 
-def postfix_eval(expr):
-    s = Stack()
+
+def is_float_int(char):
+    try:
+        float(char)
+        return True
+    except:
+        return False
+
+def validate_input(expr):
+    expr = expr.split()
+    if expr[0] in "*/+-" or is_float_int(expr[-1]) or len(expr) < 3:
+        return False
+
     for char in expr:
-        if char == " ":
+        if is_float_int(char) or char in "*/+-":
             continue
-        elif char in "*/+-":
-            op1 = str(s.pop())
-            op2 = str(s.pop())
-            s.push(eval(op2 + char + op1))
         else:
-            s.push(char)
+            return False
+    return True
+
+def postfix_eval(expr):
+    expr_val = validate_input(expr)
+    if (expr_val):
+        s = Stack()
+        for char in expr.split():
+            if char in "*/+-":
+                op1 = str(s.pop())
+                op2 = str(s.pop())
+                s.push(eval(op2 + char + op1))
+            else:
+                s.push(char)
+    if not expr_val or s.size() != 1:
+        print(f"\nERROR: Potfix expression '{expr}' failed validation.")
+        return
 
     return s.pop()
 
-expr = "456*+"
+expr = "4 5 6 * +"
 print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
 
-expr = "78+32+/"
+expr = "7 8 + 3 2 + /"
 print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
 
-expr = "93/"
+expr = "9 3 /"
+print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
+
+expr = "9a 3 /"
+print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
+
+expr = "* 3 /"
+print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
+
+expr = "3 1 / 3"
+print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
+
+expr = "3 3 3 *"
 print("Postfix Expression", expr, "evaluates to:", postfix_eval(expr))
